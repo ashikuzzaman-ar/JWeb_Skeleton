9abc72ee9841bebf2121f9f2d5103612a601ca5f
ashik <ashik@studevs>
2016-09-05 05:07:37 +0600

    Simplify Index.java controller to map both get and post method using one method.

--- src/java/com/studevs/controllers/Index.java
+++ src/java/com/studevs/controllers/Index.java
@@ -8,7 +8,6 @@ package com.studevs.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  *
@@ -17,15 +16,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class Index {
     
-    @RequestMapping(value = "index", method = RequestMethod.GET)
-    protected String doGet1(Model model){
+    @RequestMapping(value = "index")
+    protected String doRequest1(Model model){
         
-        try {
-            
-            return "index";
-        } catch (Exception e) {
-            
-            return "index";
-        }
+        return "index";
     }
 }
