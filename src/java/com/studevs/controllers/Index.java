e89388a480a2a3c5f158f9d0e90fcf6eed5873ad
ashik <ashik@studevs>
2016-09-06 07:59:36 +0600

    Implemented AJAX using JQuery and Restful Web Services.

--- src/java/com/studevs/controllers/Index.java
+++ src/java/com/studevs/controllers/Index.java
@@ -5,9 +5,13 @@
  */
 package com.studevs.controllers;
 
+import java.util.Date;
+import java.util.Random;
+import org.json.simple.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  *
@@ -15,10 +19,35 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class Index {
-    
+
     @RequestMapping(value = "index")
-    protected String doRequest1(Model model){
-        
+    public String doRequest1(Model model) {
+
         return "index";
+    }
+
+    @RequestMapping(value = "ajax_test")
+    @ResponseBody
+    public String doRequest2() {
+
+//        JSONArray array = new JSONArray();
+//        
+//        JSONObject object1 = new JSONObject();
+//        object1.put("number", String.valueOf(new Random().nextInt() * 1000)+" 1");
+//        object1.put("date", new Date().toString()+" 1");
+//        
+//        JSONObject object2 = new JSONObject();
+//        object2.put("number", String.valueOf(new Random().nextInt() * 1000)+" 2");
+//        object2.put("date", new Date().toString()+" 2");
+        
+        JSONObject object3 = new JSONObject();
+        object3.put("number", String.valueOf(new Random().nextInt() * 1000)+" 3");
+        object3.put("date", new Date().toString()+" 3");
+        
+//        array.add(object1);
+//        array.add(object2);
+//        array.add(object3);
+        
+        return object3.toJSONString();
     }
 }
--- web/WEB-INF/jsp/index.jsp
+++ web/WEB-INF/jsp/index.jsp
@@ -1,5 +1,37 @@
+<script type="text/javascript">
+    var timeInterval = 0;
+    
+    timeInterval = setInterval(ajaxTest, 2000);
+
+    function ajaxTest() {
+
+        $.ajax({
+            url: "ajax_test",
+            success: function (data) {
+                var obj1 = $.parseJSON(data);
+                
+//                for(var key in obj1){
+//                    
+//                    if(obj1.hasOwnProperty(key)){
+//                        
+//                        document.write("<h1>Generated Number: "+obj1[key].number+"</h1>");
+//                        document.write("<h1>Date: "+obj1[key].date+"</h1>");
+//                    }
+//                }
+                
+                $("#number").text(obj1.number);
+                $("#date").text(obj1.date);
+            }
+        });
+    }
+</script>
+
 <div class="container">
     <div class="jumbotron">
+
+        <h1 id="number"></h1>
+        <h1 id="date"></h1>
+
         <h1>Login Please</h1>
         <form action="home" method="POST">
             <input type="text" name="username" placeholder="Username" />
